0f099e1d80f60ef01f0f3cc720f1d99c3d64af8c
dekellevy99 <dekelllevy@gmail.com>
2022-09-24 22:47:20 +0300

    add a generateData function

--- client/src/Data.ts
+++ client/src/Data.ts
@@ -9,20 +9,32 @@ class Data{
         return [...this._players]
     }
 
-    addPlayer(player: Player){
+    addPlayer(player: Player): void{
         let existPlayer: Player|undefined = this._players.find(p => p.id === player.id)
         if (!existPlayer){
             this._players.push(player)
         }
     }
 
-    removePlayerById(playerId: number){
+    getPlayerById(playerId: number): Player|undefined{
+        let player: Player|undefined = this._players.find(p => p.id === playerId)
+        return player
+    }
+
+    removePlayerById(playerId: number): void{
         for(let i = 0; i < this._players.length; i++){
             let curPlayer = this._players[i]
             if(curPlayer.id === playerId){
                 this._players.splice(i, 1)
-
             }
+        }
+    }
+
+    async generateData(teamName: string, year: number){
+        let dataPlayers = await $.get(`http://127.0.0.1:8000/players?teamName=${teamName}&year=${year}`)
+        for(const player of dataPlayers["Players"]){
+            const {playerId, firstName, lastName, jersey, pos, picture} = player
+            this.addPlayer(new Player(playerId, firstName, lastName, jersey, pos, picture))
         }
     }
 }
