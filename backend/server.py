1862c0e9bb10b1ab3612a342316f4b9c7980760c
dekellevy99 <dekelllevy@gmail.com>
2022-09-25 14:45:25 +0300

    serve the client files(html, css, and js) in the main root

--- backend/server.py
+++ backend/server.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.responses import FileResponse
 from fastapi.staticfiles import StaticFiles
 import uvicorn
 from routers import players_api
@@ -6,9 +7,11 @@ from routers import players_api
 app = FastAPI()
 app.include_router(players_api.router)
 
+app.mount("/client/build", StaticFiles(directory="client/build"), name="static")
+
 @app.get("/")
 def root():
-    return "Server is up and running"
+    return FileResponse("./client/build/index.html")
 
 if __name__ == "__main__":
     uvicorn.run("server:app", host="0.0.0.0", port=8000, reload=True)
