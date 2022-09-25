c7df1d9d93e28dacfc52d17ae64afaed57f88dea
dekellevy99 <dekelllevy@gmail.com>
2022-09-25 16:23:54 +0300

    add a method for get the input from the user and render the data accordingally

--- client/src/main.ts
+++ client/src/main.ts
@@ -6,6 +6,14 @@
         $(this).addClass('animate');		
      }, function(){
         $(this).removeClass('animate');			
-    });	
+    });
 
+
+    $("#search-btn").on("click", function(){
+        let teamName: string = <string> $("#team-input").val()
+        let year: number = <number> $("#year-input").val()
+        data.generateData(teamName, year).then(() => {
+            render.renderData(data)
+        })
+    })
 })()
