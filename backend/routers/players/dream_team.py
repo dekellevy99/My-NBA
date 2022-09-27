e11f843f1049c211a0084af4dc03062892c95c26
dekellevy99 <dekelllevy@gmail.com>
2022-09-27 11:38:31 +0300

    create a module to manage the dream team

---
+++ backend/routers/players/dream_team.py
@@ -0,0 +1,37 @@
+NOT_FOUND = -1
+
+_dream_team_players = []
+
+def _get_player_index(player_id):
+    for i in range(len(_dream_team_players)):
+        cur_player = _dream_team_players[i]
+        if cur_player.personId == player_id:
+            return i
+    return NOT_FOUND
+
+
+def _is_in_dream_team(player):
+    player_id = player.personId
+    return _get_player_index(player_id) != NOT_FOUND
+
+
+def add_player(player):
+    if not _is_in_dream_team(player):
+        _dream_team_players.append(player)
+
+
+def get_player_by_id(player_id):
+    player_index = _get_player_index(player_id)
+    if player_index != NOT_FOUND:
+        return _dream_team_players[player_index]
+    return None
+
+
+def remove_player_by_id(player_id):
+    player_index = _get_player_index(player_id)
+    if player_index != NOT_FOUND:
+        del _dream_team_players[player_index]
+
+
+def get_dream_team():
+    return _dream_team_players[:]
