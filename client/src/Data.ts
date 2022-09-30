bcc3261249d645b0b3926db66480e9dbc7177f24
dekellevy99 <dekelllevy@gmail.com>
2022-09-30 21:53:12 +0300

    add a method doing GET http request to get the dream team

--- client/src/Data.ts
+++ client/src/Data.ts
@@ -1,31 +1,31 @@
 class Data{
-    private _players: Player[]
+    private _players: Player[];
 
     constructor(){
-        this._players = []
+        this._players = [];
     }
 
     get players(){
-        return [...this._players]
+        return [...this._players];
     }
 
     addPlayer(player: Player): void{
         let existPlayer: Player|undefined = this._players.find(p => p.id == player.id)
         if (!existPlayer){
-            this._players.push(player)
+            this._players.push(player);
         }
     }
 
     getPlayerById(playerId: number): Player|undefined{
         let player: Player|undefined = this._players.find(p => p.id == playerId)
-        return player
+        return player;
     }
 
     removePlayerById(playerId: number): void{
         for(let i = 0; i < this._players.length; i++){
             let curPlayer = this._players[i]
             if(curPlayer.id === playerId){
-                this._players.splice(i, 1)
+                this._players.splice(i, 1);
             }
         }
     }
@@ -33,19 +33,28 @@ class Data{
     async generateStatsOfPlayer(playerId: number){
         let player = this.getPlayerById(playerId)
         if (player !== undefined){
-            let firstName: string = player.firstName
-            let lastName: string = player.lastName
-            let stats = await $.get(`/players/stats/${firstName}/${lastName}`)
-            return stats
+            let firstName: string = player.firstName;
+            let lastName: string = player.lastName;
+            let stats = await $.get(`/players/stats/${firstName}/${lastName}`);
+            return stats;
         }
     }
 
-    async generateData(teamName: string, year: number){
-        let dataPlayers = await $.get(`/players?teamName=${teamName}&year=${year}`)
-        this._players.splice(0)
-        for(const player of dataPlayers["Players"]){
-            const {personId, firstName, lastName, jersey, pos, picture} = player
-            this.addPlayer(new Player(personId, firstName, lastName, jersey, pos, picture))
+    async generatePlayers(teamName: string, year: number){
+        let dataPlayers = await $.get(`/players?teamName=${teamName}&year=${year}`);
+        this._players.splice(0);
+        for(const player of dataPlayers["players"]){
+            const {personId, firstName, lastName, jersey, pos, picture} = player;
+            this.addPlayer(new Player(personId, firstName, lastName, jersey, pos, picture));
+        }
+    }
+
+    async generateDreamTeam(){
+        let dreamTeamPlayers = await $.get("/dreamTeam")
+        this._players.splice(0);
+        for(const player of dreamTeamPlayers["players"]){
+            const {personId, firstName, lastName, jersey, pos, picture} = player;
+            this.addPlayer(new Player(personId, firstName, lastName, jersey, pos, picture));
         }
     }
 }
